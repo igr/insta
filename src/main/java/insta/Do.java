2d00c1464231244d9655c52d20e817a2bd6e4ff9
Igor Spasić <igor.spasic@gmail.com>
2022-02-21 23:40:56 +0100

    Shells

--- src/main/java/insta/Do.java
+++ src/main/java/insta/Do.java
@@ -1,11 +1,14 @@
 package insta;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jodd.http.HttpRequest;
 
 import java.io.IOException;
 
 public class Do {
 	public static void main(String[] args) throws IOException {
+		checkNetOrDie();
+
 		Dotenv.configure().ignoreIfMissing()
 				.load()
 				.entries()
@@ -19,8 +22,13 @@ public class Do {
 		InstaPost instagram = new InstaPost();
 		instagram.postImage(image.toJpegBytes(), "#" + text.get());
 
+		System.out.println("Word " + text.index() + ": '" + text.get() + "' posted");
 		text.commit();
-		System.out.println("Word " + text.index() + " posted. Done.");
+		System.out.println("Done.\n\n\n");
+	}
+
+	private static void checkNetOrDie() {
+		HttpRequest.get("http://google.com").send();
 	}
 
 }
